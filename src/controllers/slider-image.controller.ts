a713114707262ea7b5ed15297346eb81d07b726b
Alexsander Alves <alvesalexsander@live.com>
2021-03-28 01:02:19 -0300

    sliderImage

--- src/controllers/slider-image.controller.ts
+++ src/controllers/slider-image.controller.ts
@@ -60,8 +60,7 @@ export class SliderImageController {
   })
   async loadJson(): Promise<void> {
     return new Promise(async (resolve, reject) => {
-      console.log(fs.readdirSync(path.resolve('./')));
-      console.log(path.resolve('./assets'));
+      console.log(fs.readdirSync(path.resolve('./src')));
       // const content = JSON.parse(fs.readFileSync(path.resolve('assets/JSON/slide.json'), {'encoding': 'utf-8'}));
       // for (const imgPath of content[0].imagens) {
       //   const found = await this.sliderImageRepository.find({where: {path: imgPath}});
